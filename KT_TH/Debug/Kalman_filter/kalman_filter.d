82cb3454fc4a1ea10e991d242a39efe35020ce83
nhom1_chhk <pdk310201@gmail.com>
2023-05-12 08:57:13 +0700

    version 2

---
+++ KT_TH/Debug/Kalman_filter/kalman_filter.d
@@ -0,0 +1,3 @@
+Kalman_filter/kalman_filter.o: G:/F401/Kalman_filter/kalman_filter.c \
+ G:/F401/Kalman_filter/kalman_filter.h
+G:/F401/Kalman_filter/kalman_filter.h:
---
+++ KT_TH/Debug/Src/main.d
@@ -0,0 +1,33 @@
+Src/main.o: ../Src/main.c \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_adc.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_tim.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h \
+ G:/F401/Kalman_filter/kalman_filter.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_adc.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_tim.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h:
+G:/F401/Kalman_filter/kalman_filter.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+G:/F401/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
